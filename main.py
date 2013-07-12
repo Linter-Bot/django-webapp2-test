56965d1931f2848190113c24934e492d2fa7eb98
sunu <sunu0000@gmail.com>
2013-07-12 19:41:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import webapp2
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from polls.models import Poll
+
+class HelloWebapp2(webapp2.RequestHandler):
+    def get(self):
+	a = Poll.objects.all()[0].question
+        self.response.write(a)
+
+app = webapp2.WSGIApplication([
+    ('/', HelloWebapp2),
+], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+    main()
