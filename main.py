d31e06bf335baeed735f29acf41e8378924dd075
DarkSunu <tarashishmishra@gmail.com>
2014-04-15 10:47:23 +0530

    Add more pep8 errors

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from polls.models import Poll
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-	a = Poll.objects.all()[0].question
+	a =Poll.objects.all()[0].question
         self.response.write(a)
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
 ], debug=True)
 
-def main():
-    from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
+test var = "i'm not gonna use this. 	This is a line with >80 chars to make pep8 linter unhappy.      "
 
-if __name__ == '__main__':
+
+
+def main():
+    from paste import httpserver                                        
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+                                         
+if __name__== '__main__':
     main()
